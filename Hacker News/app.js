04a71e60b0850499fdf9751f45a1d03e9af11d04
tldjfj123 <dksckd0804@naver.com>
2022-03-24 18:03:41 +0900

    22.03.24

--- Hacker News/app.js
+++ Hacker News/app.js
@@ -17,8 +17,19 @@ function getData(url) {
 function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newsList = [];
+    let template = `
+        <div class="container mx-auto p-4">
+            <h1>Hacker News</h1>
+            <ul>
+                {{__news_feed__}}
+            </ul>
+            <div>
+                <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+                <a href="#/page/{{__next_page__}}">다음 페이지</a>
+            </div>
+        </div>
+    `;
 
-    newsList.push('<ul>');
 
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
@@ -29,15 +40,12 @@ function newsFeed() {
             </li>
         `);
     }
-    
-    newsList.push('</ul>')
-    newsList.push(`
-        <div>
-            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
-            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
-        </div>
-    `);
-    container.innerHTML = newsList.join('')
+
+    template = template.replace('{{__news_feed__}}', newsList.join(''));
+    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 :  1);
+    template = template.replace('{{__next_page__}}', store.currentPage + 1);
+
+    container.innerHTML = template;
 }
 
 function newsDetail() {
