6ee2c59b4b5ef11dc576090ed6d4b28b42cd1194
tldjfj123 <dksckd0804@naver.com>
2022-03-24 18:13:37 +0900

    22.03.24

--- Hacker News/app.js
+++ Hacker News/app.js
@@ -1,6 +1,5 @@
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store = {
@@ -42,7 +41,7 @@ function newsFeed() {
     </div>
   `;
 
-    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
@@ -74,14 +73,58 @@ function newsFeed() {
 function newsDetail() {
   const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id))
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  container.innerHTML = `
-    <h1>${newsContent.title}</h1>
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
 
-    <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
+        {{__comments__}}
+
+      </div>
     </div>
   `;
+
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+
+    for(let i = 0; i < comments.length; i++) {
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>      
+      `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
 }
 
 function router() {
