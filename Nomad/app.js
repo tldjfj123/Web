67f746bccd5f6c59b25ae73df085c1a695e73a87
SeongJun Ahn <dksckd0804@naver.com>
2022-04-05 23:33:17 +0900

    22.04.05

---
+++ Nomad/app.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 6;
+let myName = "S"
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello! " + myName);
+
+myName = "SG";
+
+console.log("Your new name is " + myName);
